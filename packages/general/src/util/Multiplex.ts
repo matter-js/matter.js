02e5bf4b206b26079849659aa4e85ef418e12995
Greg Lauckhart <greg@lauckhart.com>
2025-08-08 22:41:45 -0700

    Device advertising refactor
    This refactors advertising logic.  This addresses a number of TODOs, adds features and generally attempts to make the logic more linear.
    
    With this change we now have "advertisers" and "advertisements".  Two advertiser implementations, `MdnsAdvertiser` and `BleAdvertiser` manage a set of `Advertisement` instances, one for each active service ("commissionable node", "operational fabric+node", etc.).
    
    `DeviceCommissioner` and `DeviceAdvertiser` previously were pretty intertwined.  Their roles are now more explicit:
    
    - The commissioner handles commissioning timeout separately from advertising
    
    - The commissioner now only deals with commissioning, not operational advertisement
    
    - The advertiser is decomposed as described above
    
    - The advertiser monitors fabrics and sessions to determine when to broadcast and/or publish (respond to queries)
    
    - Advertisements act independently so e.g. a node may have simultaneously:
    
      - An associated `BleAdvertisement` and `CommissionableMdnsAdvertisement` broadcasting
    
      - An `OperationalMdnsAdvertisement` for fabric 1 published (responding to queries) but not actively broadcasting
    
      - An `OperationalMdnsAdvertisement` for fabric 2 published and broadcasting at a fast rate
    
      - An `OperationalMdnsAdvertisement` for fabric 3 published but broadcasting at a slower rate
    
    Additional MDNS updates include:
    
    - Extended advertisement with masking of vendor/product
    
    - Support for TCP and ICD flags
    
    - A configurable schedule for repeated MDNS broadcasts with fast defaults that align to the DNS-SD spec
    
    - Logic to pause/resume advertising based on session establishment on a per-service basis
    
    - More aggressive (but configurable) advertising when we retry due to missed ACKs
    
    - `MdnsSocket` shared between MDNS clients and servers to cut down duplicate code and avoid processing packets twice
    
    - Record generation is decomposed to make semantics clear and reduce code duplication between the different types of advertisements
    
    - Reworked tests to reduce code duplication and remove yields so they are not tick-dependent
    
    On the BLE side:
    
    - BLE broadcast rates change over time; this is configurable with defaults that advertise as frequently as possible while conforming to Matter specs
    
    - Support for extended advertisement with masking of vendor/product/aad
    
    - Modified the `Ble` interface to be lower level with abstractions for the central and peripheral interfaces.  This allows higher-level logic (such as features above) to reside in @matter/protocol so we don't need to copy between platforms

--- packages/general/src/util/Multiplex.ts
+++ packages/general/src/util/Multiplex.ts
@@ -4,11 +4,62 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { Logger } from "#log/Logger.js";
+import { MatterAggregateError } from "#MatterError.js";
+import { MaybePromise } from "./Promises.js";
+import { BasicSet } from "./Set.js";
+
+const logger = Logger.get("Multiplex");
+
 /**
  * A "multiplex" tracks an extensible set of promises.
  */
 export interface Multiplex {
-    add(worker: Promise<unknown>): void;
+    add(worker: Promise<unknown>, description?: string): void;
     close(): Promise<void>;
     [Symbol.asyncDispose](): Promise<void>;
+}
+
+/**
+ * A basic multiplex that tracks all promises given to it.
+ */
+export class BasicMultiplex implements PromiseLike<void> {
+    #workers = new BasicSet<Promise<unknown> & { description?: string }>();
+
+    add(worker: MaybePromise<unknown>, description?: string) {
+        if (!MaybePromise.is(worker)) {
+            return;
+        }
+
+        const promise = Promise.resolve(worker)
+            .catch(e => {
+                let message = "Unhandled error";
+                if (description) {
+                    message = `${message} in ${description}`;
+                }
+                logger.error(message, e);
+            })
+            .finally(() => this.#workers.delete(promise)) as Promise<unknown> & { description?: string };
+
+        if (description !== undefined) {
+            promise.description = description;
+        }
+
+        this.#workers.add(promise);
+    }
+
+    async close() {
+        while (this.#workers.size) {
+            await MatterAggregateError.allSettled(this.#workers);
+        }
+    }
+
+    then<TResult1 = void, TResult2 = never>(
+        onfulfilled?: ((value: void) => TResult1 | PromiseLike<TResult1>) | null,
+        onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null,
+    ): PromiseLike<TResult1 | TResult2> {
+        return this.close().then(onfulfilled, onrejected);
+    }
+
+    [Symbol.asyncDispose] = this.close.bind(this);
 }
