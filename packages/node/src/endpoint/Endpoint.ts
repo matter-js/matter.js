069878f48d1243e9365596fb630079f5a3e18ae9
Greg Lauckhart <greg@lauckhart.com>
2025-10-06 16:20:57 -0700

    Streaming state component
    * Adds a new high-level `StateStream` component that provides a stream of changes for a node
    
    * The stream provides direct access to the node/endpoint/behavior/attributes that changed
    
    * It will emit changes both for ServerNode and any active ClientNodes
    
    * A new `ChangeNotificationService` acts as the intermediary between `StateStream` and other components
    
    * There are only two types of state events, "update" and "delete":
    
      - `update` provides a list of attributes and associated values for a single behavior
    
      - `delete` indicates that an endpoint (possibly a ClientNode) was deleted
    
    * The stream supports limited filtering by node and/or cluster
    
    * The stream also accepts initial data versions.  All data streams initially unless the data is at the specified version
    
    * Also includes an update to `ServerNode` that will stop `MdnsService` if the node owns it (that is, it cannot be shared across nodes)
    
    * Changes for Q attributes should adhere to the quiet broadcast schedule
    
    * Change collection has a configurable coalescence interval to reduce the number of events emitted

--- packages/node/src/endpoint/Endpoint.ts
+++ packages/node/src/endpoint/Endpoint.ts
@@ -21,9 +21,9 @@ import {
     UninitializedDependencyError,
 } from "#general";
 import { DataModelPath } from "#model";
+import { ProtocolService } from "#node/integration/ProtocolService.js";
 import type { Node } from "#node/Node.js";
 import { IdentityService } from "#node/server/IdentityService.js";
-import { ProtocolService } from "#node/server/ProtocolService.js";
 import { Val } from "#protocol";
 import { EndpointNumber } from "#types";
 import { RootEndpoint } from "../endpoints/root.js";
@@ -227,7 +227,7 @@ export class Endpoint<T extends EndpointType = EndpointType.Empty> {
 
                 await tx.addResources(behavior);
 
-                const patch = (behavior.constructor as Behavior.Type).supervisor.patch;
+                const patch = behavior.type.supervisor.patch;
 
                 if (typeof vals !== "object") {
                     throw new ImplementationError(
@@ -266,7 +266,7 @@ export class Endpoint<T extends EndpointType = EndpointType.Empty> {
             await tx.begin();
             await tx.addResources(behavior);
 
-            const patch = (behavior.constructor as Behavior.Type).supervisor.patch;
+            const patch = behavior.type.supervisor.patch;
 
             if (typeof values !== "object") {
                 throw new ImplementationError(`State values for ${type.id} must be an object, not ${typeof values}`);
