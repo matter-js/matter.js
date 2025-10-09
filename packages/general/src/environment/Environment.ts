1a0c82fc1669c493c5096647ddc0d86d6d777af0
Ingo Fischer <github@fischer-ka.de>
2025-10-09 12:22:40 +0200

    adjustment

--- packages/general/src/environment/Environment.ts
+++ packages/general/src/environment/Environment.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { InternalError } from "#MatterError.js";
+import { ImplementationError, InternalError } from "#MatterError.js";
 import { Instant } from "#time/TimeUnit.js";
 import { MaybePromise } from "#util/Promises.js";
 import { DiagnosticSource } from "../log/DiagnosticSource.js";
@@ -113,35 +113,14 @@ export class Environment {
      * @param instance optional instance expected, if existing instance does not match it is not deleted
      */
     delete(type: Environmental.ServiceType, instance?: any) {
-        const knownInstance = this.#services?.get(type);
-        if (instance !== undefined) {
-            if (knownInstance !== instance) {
+        const localInstance = this.#services?.get(type);
+        if (localInstance !== undefined) {
+            if (instance !== undefined && localInstance !== instance) {
                 return;
             }
-        } else {
-            instance = knownInstance;
-        }
-        this.#services?.delete(type);
-        this.#parent?.delete(type, instance);
 
-        this.#deleted.emit(type, instance);
-
-        const serviceEvents = this.#serviceEvents.get(type);
-        if (serviceEvents) {
-            serviceEvents.deleted.emit(instance);
-        }
-    }
-
-    /**
-     * Prevent this environment from automatically instantiating or retrieving a service from parent environment.
-     *
-     * @param type the class of the service to block
-     */
-    block(type: Environmental.ServiceType) {
-        const instance = this.#services?.get(type);
-
-        if (instance !== undefined && instance !== null) {
             this.#services?.delete(type);
+            this.block(type);
 
             this.#deleted.emit(type, instance);
 
@@ -149,6 +128,24 @@ export class Environment {
             if (serviceEvents) {
                 serviceEvents.deleted.emit(instance);
             }
+        } else if (this.#parent !== undefined) {
+            this.#parent.delete(type, instance);
+        }
+    }
+
+    /**
+     * Prevent this environment from automatically instantiating or retrieving a service from parent environment.
+     * If a local instance exists it is removed first.
+     *
+     * @param type the class of the service to block
+     */
+    block(type: Environmental.ServiceType) {
+        const instance = this.#services?.get(type);
+
+        if (instance !== undefined && instance !== null) {
+            throw new ImplementationError(
+                `Cannot block service ${type.name} because it is currently installed. Please delete it first.`,
+            );
         }
 
         this.#services?.set(type, null);
