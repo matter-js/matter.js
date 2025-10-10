daf1bdffce6e494f9bbd54729e8fb56255f8ce4c
Greg Lauckhart <greg@lauckhart.com>
2025-10-09 17:56:20 -0700

    CHIP container testing updates
    Includes temporarily disabling OPCREDS/3.4 due to bugs.

--- support/chip-testing/test/core/OPCREDS.test.ts
+++ support/chip-testing/test/core/OPCREDS.test.ts
@@ -6,6 +6,10 @@
 
 describe("OPCREDS", () => {
     chip("OPCREDS/*").exclude(
+        // Temporarily exclude pending https://github.com/project-chip/connectedhomeip/pull/41374
+        // TODO - resurrect once above is addressed
+        "OPCREDS/3.4",
+
         // These are Matter 1.5
         "OPCREDS/3.8",
         "OPCREDS/3.9",
