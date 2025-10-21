3bcb27dfed5b5309d7b554c073fe4d635af7dba9
Ingo Fischer <github@fischer-ka.de>
2025-10-21 09:06:53 +0200

    try with a shell wrapper

--- support/chip-testing/src/AllClustersTestApp.ts
+++ support/chip-testing/src/AllClustersTestApp.ts
@@ -7,13 +7,9 @@
 
 import "@matter/nodejs";
 
-import { Logger } from "@matter/general";
-import { createFileLogger } from "@matter/nodejs";
 import { AllClustersTestInstance } from "./AllClustersTestInstance.js";
 import { startDeviceTestApp } from "./GenericTestApp.js";
 import { StorageBackendAsyncJsonFile } from "./storage/StorageBackendAsyncJsonFile.js";
-
-Logger.destinations.default.write = await createFileLogger("./test_allclusters.log");
 
 process.title = "AllClustersTestApp.js"; // Needed for Stress testing to detect the process to kill.
 
