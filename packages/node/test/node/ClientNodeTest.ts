e3e2a37c24fde08b942df4404a1f09ed782826d1
Ingo Fischer <github@fischer-ka.de>
2025-12-10 18:39:52 +0100

    Adjust behavior endpoint recreation issue

--- packages/node/test/node/ClientNodeTest.ts
+++ packages/node/test/node/ClientNodeTest.ts
@@ -428,6 +428,14 @@ describe("ClientNode", () => {
         expect((state as Val.Struct).attr$1).equals("hello");
     });
 
+    it("handles structure change and cluster recreation while device online", () => {
+        // TODO
+    });
+
+    it("handles structure change and cluster recreation while device offline", () => {
+        // TODO
+    });
+
     it("handles shutdown event and reestablishes connection", () => {
         // TODO
     });
