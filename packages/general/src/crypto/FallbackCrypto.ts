ba090ec6cc1ff965d1faad4c8ac857c8d44c436a
craftingmod <alyac1234@gmail.com>
2026-01-13 02:09:24 +0900

    Chore: match codestyle of `FallbackCrypto.ts`.

--- packages/general/src/crypto/FallbackCrypto.ts
+++ packages/general/src/crypto/FallbackCrypto.ts
@@ -24,35 +24,38 @@ import { Crypto } from "@matter/general";
  * it automatically retries the operation using the fallback implementation.
  */
 export class FallbackCrypto extends Crypto {
-  constructor(
-    protected primaryCrypto: Crypto,
-    protected fallbackCrypto: Crypto,
-    public override readonly implementationName = "FallbackCrypto"
-  ) {
-    super()
+  implementationName = "FallbackCrypto";
+  #primaryCrypto: Crypto;
+  #fallbackCrypto: Crypto;
+
+  constructor(primaryCrypto: Crypto, fallbackCrypto: Crypto) {
+    super();
+
+    this.#primaryCrypto = primaryCrypto;
+    this.#fallbackCrypto = fallbackCrypto;
   }
 
   encrypt(key: Bytes, data: Bytes, nonce: Bytes, aad?: Bytes): Bytes {
     try {
-      return this.primaryCrypto.encrypt(key, data, nonce, aad);
+      return this.#primaryCrypto.encrypt(key, data, nonce, aad);
     } catch (err) {
-      return this.fallbackCrypto.encrypt(key, data, nonce, aad);
+      return this.#fallbackCrypto.encrypt(key, data, nonce, aad);
     }
   }
 
   decrypt(key: Bytes, encrypted: Bytes, nonce: Bytes, aad?: Bytes): Bytes {
     try {
-      return this.primaryCrypto.decrypt(key, encrypted, nonce, aad);
+      return this.#primaryCrypto.decrypt(key, encrypted, nonce, aad);
     } catch (err) {
-      return this.fallbackCrypto.decrypt(key, encrypted, nonce, aad);
+      return this.#fallbackCrypto.decrypt(key, encrypted, nonce, aad);
     }
   }
 
   randomBytes(length: number): Bytes {
     try {
-      return this.primaryCrypto.randomBytes(length)
+      return this.#primaryCrypto.randomBytes(length);
     } catch (err) {
-      return this.fallbackCrypto.randomBytes(length)
+      return this.#fallbackCrypto.randomBytes(length);
     }
   }
 
@@ -61,65 +64,65 @@ export class FallbackCrypto extends Crypto {
     algorithm?: HashAlgorithm,
   ): MaybePromise<Bytes> {
     try {
-      return this.primaryCrypto.computeHash(data, algorithm);
+      return this.#primaryCrypto.computeHash(data, algorithm);
     } catch (err) {
-      return this.fallbackCrypto.computeHash(data, algorithm);
+      return this.#fallbackCrypto.computeHash(data, algorithm);
     }
   }
 
   createPbkdf2Key(secret: Bytes, salt: Bytes, iteration: number, keyLength: number): MaybePromise<Bytes> {
     try {
-      return this.primaryCrypto.createPbkdf2Key(secret, salt, iteration, keyLength);
+      return this.#primaryCrypto.createPbkdf2Key(secret, salt, iteration, keyLength);
     } catch (err) {
-      return this.fallbackCrypto.createPbkdf2Key(secret, salt, iteration, keyLength);
+      return this.#fallbackCrypto.createPbkdf2Key(secret, salt, iteration, keyLength);
     }
   }
 
   createHkdfKey(secret: Bytes, salt: Bytes, info: Bytes, length?: number): MaybePromise<Bytes> {
     try {
-      return this.primaryCrypto.createHkdfKey(secret, salt, info, length);
+      return this.#primaryCrypto.createHkdfKey(secret, salt, info, length);
     } catch (err) {
-      return this.fallbackCrypto.createHkdfKey(secret, salt, info, length);
+      return this.#fallbackCrypto.createHkdfKey(secret, salt, info, length);
     }
   }
 
   signHmac(key: Bytes, data: Bytes): MaybePromise<Bytes> {
     try {
-      return this.primaryCrypto.signHmac(key, data);
+      return this.#primaryCrypto.signHmac(key, data);
     } catch (err) {
-      return this.fallbackCrypto.signHmac(key, data);
+      return this.#fallbackCrypto.signHmac(key, data);
     }
   }
 
   signEcdsa(privateKey: JsonWebKey, data: Bytes | Bytes[]): MaybePromise<EcdsaSignature> {
     try {
-      return this.primaryCrypto.signEcdsa(privateKey, data);
+      return this.#primaryCrypto.signEcdsa(privateKey, data);
     } catch (err) {
-      return this.fallbackCrypto.signEcdsa(privateKey, data);
+      return this.#fallbackCrypto.signEcdsa(privateKey, data);
     }
   }
 
   verifyEcdsa(publicKey: JsonWebKey, data: Bytes, signature: EcdsaSignature): MaybePromise<void> {
     try {
-      return this.primaryCrypto.verifyEcdsa(publicKey, data, signature);
+      return this.#primaryCrypto.verifyEcdsa(publicKey, data, signature);
     } catch (err) {
-      return this.fallbackCrypto.verifyEcdsa(publicKey, data, signature);
+      return this.#fallbackCrypto.verifyEcdsa(publicKey, data, signature);
     }
   }
 
   createKeyPair(): MaybePromise<PrivateKey> {
     try {
-      return this.primaryCrypto.createKeyPair();
+      return this.#primaryCrypto.createKeyPair();
     } catch (err) {
-      return this.fallbackCrypto.createKeyPair();
+      return this.#fallbackCrypto.createKeyPair();
     }
   }
 
   generateDhSecret(key: PrivateKey, peerKey: PublicKey): MaybePromise<Bytes> {
     try {
-      return this.primaryCrypto.generateDhSecret(key, peerKey);
+      return this.#primaryCrypto.generateDhSecret(key, peerKey);
     } catch (err) {
-      return this.fallbackCrypto.generateDhSecret(key, peerKey);
+      return this.#fallbackCrypto.generateDhSecret(key, peerKey);
     }
   }
 }
