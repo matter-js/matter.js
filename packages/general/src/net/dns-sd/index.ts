d88cc1a1163c0ad22d5a21c9024ae69a8a9f4a9b
lauckhart <greg@lauckhart.com>
2026-01-26 11:24:21 -0800

    DNS-SD client (#3075)
    * DNS-SD client
    
    This is a replacement for current `MdnsClient` class as well as some other pieces of current networking stack.
    
    Rough architecture:
    
      * `DnssdNames` provides centralized access to resolved DNS-SD names and manages:
    
        * The `MdnsSocket` used for communication
    
        * `DnssdName` is an observable representation of the known state of a single DNS-SD name
    
        * `DnssdSolicitor` implements active discovery (via query) of name records
    
      * `IpService` builds on former to provide a higher-level observable representation of a single DNS-SD service.  It manages:
    
        * `DnssdName` instances associated with the service and any hostnames identified by PTR records
    
        * `IpServiceResolution` manages the process of locating IP addresses using cached state and `DnssdSolicitor`
    
        * `IpServiceStatus` tracks connection attempts, records current state of the service, and initiates resolution as necessary
    
    The public interface currently ties directly to our own DNS-SD implementation but I tried to structure so that we can add Bonjour, Avahi etc. implementations later.
    
    * Port optimizations from #3075
    
    * .kvs -> .parameters
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/general/src/net/dns-sd/index.ts
+++ packages/general/src/net/dns-sd/index.ts
@@ -4,4 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+export * from "./DnssdName.js";
+export * from "./DnssdNames.js";
+export * from "./DnssdSolicitor.js";
+export * from "./IpService.js";
+export * from "./IpServiceResolution.js";
+export * from "./IpServiceStatus.js";
 export * from "./MdnsSocket.js";
