22969fa61879386a0f8b2d2b95951e61b3dedfdd
Ingo Fischer <github@fischer-ka.de>
2026-01-29 14:05:37 +0100

    complete dcl error codes

--- packages/protocol/src/dcl/DclRestApiTypes.ts
+++ packages/protocol/src/dcl/DclRestApiTypes.ts
@@ -7,10 +7,27 @@
 import { DeviceModelDclSchema, DeviceSoftwareVersionModelDclSchema, ProductAttestationDclSchema } from "#types";
 
 /**
- * Currently incomplete List of DCL Error codes
+ * DCL Error codes
+ * from https://pkg.go.dev/google.golang.org/grpc@v1.60.1/codes
  */
 export enum DclErrorCodes {
+    Ok = 0,
+    Canceled = 1,
+    Unknown = 2,
+    InvalidArgument = 3,
+    DeadlineExceeded = 4,
     NotFound = 5,
+    AlreadyExists = 6,
+    PermissionDenied = 7,
+    ResourceExhausted = 8,
+    FailedPrecondition = 9,
+    Aborted = 10,
+    OutOfRange = 11,
+    Unimplemented = 12,
+    Internal = 13,
+    Unavailable = 14,
+    DataLoss = 15,
+    Unauthenticated = 16,
 }
 
 export interface DclApiErrorResponse {
