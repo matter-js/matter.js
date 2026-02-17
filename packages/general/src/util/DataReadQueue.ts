d2138f00f27767fa240dd8a77546a202391f06f1
Greg Lauckhart <greg@lauckhart.com>
2026-02-17 08:44:20 -0800

    Throw cause when rejecting in read queue

--- packages/general/src/util/DataReadQueue.ts
+++ packages/general/src/util/DataReadQueue.ts
@@ -87,7 +87,7 @@ export class DataReadQueue<T> {
         this.#pendingRead = undefined;
 
         if (pendingRead) {
-            pendingRead.rejecter(new EndOfStreamError());
+            pendingRead.rejecter(cause);
         }
     }
 }
