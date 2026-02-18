c3f9d88bcac9190721737482cd36a7681157be71
Ingo Fischer <github@fischer-ka.de>
2026-02-18 20:45:33 +0100

    Remove ICAC workaround added in 0.16.0
    because test now landed in SDK (https://github.com/project-chip/connectedhomeip/pull/43030)

--- packages/protocol/src/fabric/Fabric.ts
+++ packages/protocol/src/fabric/Fabric.ts
@@ -524,13 +524,6 @@ export class FabricBuilder {
             throw new MatterFlowError("Root certificate needs to be set first");
         }
 
-        // Workaround for an issue with the Ikea hub where the root certificate was also provided as ICAC
-        // see https://github.com/project-chip/connectedhomeip/issues/42479
-        if (intermediateCACert !== undefined && Bytes.areEqual(this.#rootCert, intermediateCACert)) {
-            logger.info("Intermediate CA certificate is identical to root certificate; omitting ICAC");
-            intermediateCACert = undefined;
-        }
-
         const rootCert = Rcac.fromTlv(this.#rootCert);
         const nocCert = Noc.fromTlv(operationalCert);
         const icaCert = intermediateCACert !== undefined ? Icac.fromTlv(intermediateCACert) : undefined;
