9672fe17e9e1c22ed3aa8c70c3c2de598762fc2d
Ingo Fischer <github@fischer-ka.de>
2026-02-27 18:25:55 +0100

    Adjust tests

--- packages/node/test/node/EventReadResponseTest.ts
+++ packages/node/test/node/EventReadResponseTest.ts
@@ -338,7 +338,7 @@ describe("EventReadResponse", () => {
         expect(response.counts).deep.equals({
             status: 0,
             success: ROOT_ENDPOINT_FULL_CLUSTER_LIST_COUNT,
-            existent: ROOT_ENDPOINT_FULL_CLUSTER_LIST_COUNT + 3,
+            existent: ROOT_ENDPOINT_FULL_CLUSTER_LIST_COUNT + 4,
         });
     });
 
