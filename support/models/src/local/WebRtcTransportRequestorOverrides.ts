5af7795eec9e8c4028552ed8e650f59e2ff4c3b1
Ingo Fischer <github@fischer-ka.de>
2026-03-19 14:15:17 +0100

    Adjusted overrides

---
+++ support/models/src/local/WebRtcTransportRequestorOverrides.ts
@@ -0,0 +1,33 @@
+/**
+ * @license
+ * Copyright 2022-2026 Matter.js Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { LocalMatter } from "../local.js";
+
+// WebRTCSessionStruct and ICEServerStruct are defined globally at §11.4 but reference cross-cluster types.
+// patchIllegalCrossClusterReferences only handles cluster-internal references, so we override the affected fields here.
+LocalMatter.children.push({
+    tag: "cluster",
+    name: "WebRtcTransportRequestor",
+    asOf: "1.5",
+
+    children: [
+        {
+            tag: "datatype",
+            name: "WebRTCSessionStruct",
+            type: "struct",
+            children: [
+                { tag: "field", id: 0x4, name: "VideoStreamId", type: "CameraAvStreamManagement.VideoStreamID" },
+                { tag: "field", id: 0x5, name: "AudioStreamId", type: "CameraAvStreamManagement.AudioStreamID" },
+            ],
+        },
+        {
+            tag: "datatype",
+            name: "ICEServerStruct",
+            type: "struct",
+            children: [{ tag: "field", id: 0x3, name: "Caid", type: "TlsCertificateManagement.TLSCAID" }],
+        },
+    ],
+});
